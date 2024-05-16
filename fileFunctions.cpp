29d5671259625e84b15f6a01dd6e6f96e6915994
ashen5ocdsb <ashen5@ocdsb.ca>
2024-05-16 12:23:34 -0400

    Update fileFunctions.cpp

--- fileFunctions.cpp
+++ fileFunctions.cpp
@@ -3,9 +3,14 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#define COLOUR al_map_rgb(200, 200, 0)
+
 
 // Prototypes
-void printImage (int x, int y);
+void printImage (int x, int y, int file);
 
 // Define screen size
 const int SCREEN_W = 1920;
@@ -32,7 +37,16 @@ int main(){
     // create rectangle
     al_clear_to_color(al_map_rgb(0, 0, 0));\
     // set spot and colour
-    al_draw_rectangle(90, 90, 200, 200,al_map_rgb(200, 200, 0) , 10);
+    al_draw_rectangle(90, 150, 200, 260,al_map_rgb(200, 200, 0) , 10);
+    // rectangle 2
+    al_draw_rectangle(90, 330, 200, 440,al_map_rgb(200, 200, 0) , 10);
+    // rectangle 3
+    al_draw_rectangle(90, 510, 200, 620,al_map_rgb(200, 200, 0) , 10);
+    // rectangle 4
+    al_draw_rectangle(90, 690, 200, 800,al_map_rgb(200, 200, 0) , 10);
+    // rectangle 5
+    al_draw_rectangle(90, 870, 200, 980,al_map_rgb(200, 200, 0) , 10);
+
     // flip display to show image
     al_flip_display();
 
@@ -43,34 +57,68 @@ int main(){
     	return -1;
 	}
 
-    // print image - give variables
-    printImage (115, 120);
+    // print image - give variables 180 difference in Y
+    printImage (113, 180, 1);
+    printImage (95, 335, 2);
+    printImage (113, 540, 2);
+    printImage (113, 720, 1);
+    printImage (113, 900, 1);
 
-    // create peramiters 
+
+    // text
+
+       al_init_font_addon(); // initialize the font addon
+       al_init_ttf_addon();// initialize the ttf (True Type Font) addon
+
+       // load the specific font you want
+       ALLEGRO_FONT *font = al_load_ttf_font("comic.ttf", 50, 0);
+       if (!font){
+          al_show_native_message_box(display, "Error", "Error", "Could not load comic.ttf",
+                                        nullptr, ALLEGRO_MESSAGEBOX_ERROR);
+          return -1;
+       }
+
+       al_draw_text(font, COLOUR, 145, 60, ALLEGRO_ALIGN_CENTRE, "Shirts");
+       al_flip_display();
+
+
+    // MOUSE FUNCTIONS
     while(!select){
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
-        if(ev.mouse.x >= 90 && ev.mouse.y >= 90 && ev.mouse.x <= 200 && ev.mouse.y <= 200 && ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+        if(ev.mouse.x >= 90 && ev.mouse.y >= 150 && ev.mouse.x <= 200 && ev.mouse.y <= 260 && ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
         printf("Hooray!(%d, %d)", ev.mouse.x, ev.mouse.y);
-        printImage (910, 500);
+        printImage (910, 500, 1);
+
+       }else if (ev.mouse.x >= 90 && ev.mouse.y >= 330 && ev.mouse.x <= 200 && ev.mouse.y <= 440 && ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+        printf("Hooray!(%d, %d)", ev.mouse.x, ev.mouse.y);
+        printImage (700, 500, 2);
 
     } else if(ev.mouse.x >= 90 && ev.mouse.y >= 90 && ev.mouse.x <= 200 && ev.mouse.y <= 200)
         printf("(%d, %d)", ev.mouse.x, ev.mouse.y);
+
+
     }
+
+
 
 return 0;
 }
 
 
-void printImage (int x, int y){
+void printImage (int x, int y, int file){
 
     // declare and initialize image and display
     ALLEGRO_DISPLAY *display = nullptr;
     ALLEGRO_BITMAP *image = nullptr;
 
 	// assign file to image
-	image = al_load_bitmap("rainbow2.bmp");
-  	
+	if (file == 1){
+        image = al_load_bitmap("rainbow2.bmp");
+	} else if (file == 2){
+        image = al_load_bitmap("redshirt.png");
+        }
+
   	// error message
   	if (!image) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to load image!", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
@@ -86,133 +134,4 @@ void printImage (int x, int y){
 }
 
 
-
-
-/*
-	#include <allegro5\allegro.h>
-#include <allegro5\allegro_primitives.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_native_dialog.h>
-//Code sourced from Mike Geig
-int mouseInput(void){
-    int width = 640;
-	int height = 480;
-
-	bool done = false;
-	bool draw = true;
-	int pos_x = width / 2;
-	int pos_y = height / 2;
-
-	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-
-	if(!al_init())										//initialize Allegro
-		return -1;
-
-	display = al_create_display(width, height);			//create our display object
-
-	if(!display)										//test display object
-		return -1;
-
-
-	al_init_primitives_addon();
-	al_install_mouse();
-
-	event_queue = al_create_event_queue();
-
-	al_register_event_source(event_queue, al_get_mouse_event_source());
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-
-	al_hide_mouse_cursor(display);
-	while(!done)
-	{
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
-
-		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-		{
-			done = true;
-		}
-		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
-		{
-			if(ev.mouse.button & 1)
-				draw = !draw;
-			else if (ev.mouse.button & 2)
-				done = true;
-		}
-		else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES)
-		{
-			pos_x = ev.mouse.x;
-			pos_y = ev.mouse.y;
-		}
-
-		if(draw)
-			al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(255, 0, 255));
-		al_flip_display();
-		al_clear_to_color(al_map_rgb(0,0,0));
-	}
-
-	al_destroy_event_queue(event_queue);
-	al_destroy_display(display);						//destroy our display object
-
-	return 0;
-}
-
-// You must include the allegro header files
-
-
-const int SCREEN_W = 640;       // screen width
-const int SCREEN_H = 480;       // screen height
-
-int displayScreen() {
-	ALLEGRO_DISPLAY *display = nullptr;
-
-	// Initialize Allegro
-	al_init();
-
-	// Initialize display
-
-	display = al_create_display(SCREEN_W, SCREEN_H);
-	if (!display) {
-    	al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-       	return -1;
-	}
-
-	// Initialize image add on
- 	if (!al_init_image_addon()) {
-    	al_show_native_message_box(display, "Error", "Error",
-    		"Failed to initialize image addon!", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-    	return -1;
-	}
-
-	// Declare a BITMAP called image, setting it's initial value to nullptr
-	ALLEGRO_BITMAP *image = nullptr;
-
-	// Load the bitmap into the Bitmap structure
-	// image file must be in same directory.
-	// Particularly check return code of this type of function that will fail if file not found.
-	image = al_load_bitmap("11person.bmp");
-  	if (!image) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-      	al_destroy_display(display);
-     	return -1;
-	 }
-
-	// Display picture started at top left corner
-	al_draw_bitmap(image, 0,  0, 0);
-
-	// write display to screen
-	al_flip_display();
-
-    // Wait 5 seconds
-	//al_rest(5);
-
-	// Free memory and return with successful return code..
-	//al_destroy_display(display);
-   	//al_destroy_bitmap(image);
-	return 0;
-}
-*/ 
 
