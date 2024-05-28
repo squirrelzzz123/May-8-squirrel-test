632d07b9cacd0ee61d5b2f57dc0bc26685f8d265
ninatouchgrass <nina.tseng314@gmail.com>
2024-05-28 13:29:27 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -27,24 +27,21 @@ int loadText(){
         // text
        al_init_font_addon(); // initialize the font addon
        al_init_ttf_addon();// initialize the ttf (True Type Font) addon
-
        // load the specific font you want
        ALLEGRO_FONT *font = al_load_ttf_font("superLarky.ttf", 40, 0);
        if (!font){
-          al_show_native_message_box(display, "Error", "Error", "Could not load comic.ttf",
-                                        nullptr, ALLEGRO_MESSAGEBOX_ERROR);
+          al_show_native_message_box(display, "Error", "Error", "Could not load comic.ttf", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
           return -1;
        }
        al_draw_text(font, COLOUR, 145, 60, ALLEGRO_ALIGN_CENTRE, "Shirts");
        al_draw_text(font, COLOUR, 320, 60, ALLEGRO_ALIGN_CENTRE, "Pants");
        al_draw_text(font, COLOUR, 1581, 60, ALLEGRO_ALIGN_CENTRE, "Shoes");
        al_draw_text(font, COLOUR, 1756, 60, ALLEGRO_ALIGN_CENTRE, "Others");
-
+        //Flip to display
        al_flip_display();
 }
 
 void printImage (int x, int y, int file){
-
     // declare and initialize image and display
     ALLEGRO_DISPLAY *display = nullptr;
     ALLEGRO_BITMAP *image = nullptr;
@@ -64,52 +61,40 @@ void printImage (int x, int y, int file){
         image = al_load_bitmap("loseScreen.png");
     } else if (file == 7){
         image = al_load_bitmap("winScreen.png");
-
     }
-
   	// error message
   	if (!image) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to load image!", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
       	al_destroy_display(display);
 	 }
-
 	// print image based on coordinates given
 	al_draw_bitmap(image, x, y, 0);
-
 	// write display to screen
 	al_flip_display();
 }
 
 //prints out the score to the screen
-//superLarky.ttf
-//comic.ttf
 int printScore(int total){
         //print images of rectangles for the scores
        printImage(1920/2-410, 25, 3);
-       //printImage(1920/2-123, 25, 3);
        // load the specific font you want
        ALLEGRO_FONT *font = al_load_ttf_font("superLarky.ttf", 30, 0);
        if (!font){
           al_show_native_message_box(display, "Error", "Error", "Could not load comic.ttf", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
           return -1;
        }
-
        al_draw_text(font, COLOUR, 1920/2-200, 25, ALLEGRO_ALIGN_CENTRE, "Current Score");
        al_draw_text(font, COLOUR, 1920/2+80, 25, ALLEGRO_ALIGN_CENTRE, "High Score");
        //print current score
        char str[10];
         sprintf(str, "%d", total);
        al_draw_text(font, COLOUR, 1920/2-203, 73, ALLEGRO_ALIGN_CENTRE, str);
-       //print highest score
-        //sprintf(str, "%d", high);
-       //al_draw_text(font, COLOUR, 1044, 73, ALLEGRO_ALIGN_CENTRE, str);
        al_flip_display();
 }
 
 //print the highest score from the textfile
 int printHighScore(int high){
         //print images of rectangles for the scores
-       //printImage(1920/2-410, 25, 3);
        printImage(1920/2-123, 25, 3);
        // load the specific font you want
        ALLEGRO_FONT *font = al_load_ttf_font("superLarky.ttf", 30, 0);
@@ -117,15 +102,11 @@ int printHighScore(int high){
           al_show_native_message_box(display, "Error", "Error", "Could not load comic.ttf", nullptr, ALLEGRO_MESSAGEBOX_ERROR);
           return -1;
        }
-
        al_draw_text(font, COLOUR, 1920/2-200, 25, ALLEGRO_ALIGN_CENTRE, "Current Score");
        al_draw_text(font, COLOUR, 1920/2+80, 25, ALLEGRO_ALIGN_CENTRE, "High Score");
        //print current score
        char str[10];
         sprintf(str, "%d", high);
-       //al_draw_text(font, COLOUR, 1920/2-203, 73, ALLEGRO_ALIGN_CENTRE, str);
-       //print highest score
-        //sprintf(str, "%d", high);
        al_draw_text(font, COLOUR, 1044, 73, ALLEGRO_ALIGN_CENTRE, str);
        al_flip_display();
 }
@@ -138,33 +119,6 @@ int returnHighScore(int &highScore, int current){
         return highScore;
     }
 }
-//delete if not needed THIS FUNCTION IS NOT CALLED
-//saves the highest score into the textfile
-int scoreCheck (int highScore){
-    // initialize and open file variable
-    FILE * fptr;
-    int score2;
-    fptr = fopen("scoreCheck.txt", "r");
-    fscanf(fptr, "%d", score2);
-    //printf("%d", score2);
-
-    // error message
-    if (fptr == nullptr) {
-	    return 1;
-	}
-
-    fscanf(fptr, "%d", score2);
-    printf("%d", score2);
-
-    if (highScore > score2){
-        score2 = highScore;
-        printf ("gottem");
-    }
-    fclose(fptr);
-
-    //printf("%d ", score2);
-    //return score2;
-}
 
 int printIntoFile(int &highScore){
     FILE *fptr;
@@ -175,7 +129,6 @@ int printIntoFile(int &highScore){
 
 // rectangle function (increase by 180)
 void displayRectangles (){
-
     // For shirts
     for (int i = 0; i < 5; i ++){
         al_draw_rectangle(90, 150 + (i*180), 200, 260 + (i*180), al_map_rgb(200, 200, 0) , 7);
@@ -193,15 +146,12 @@ void displayRectangles (){
         al_draw_rectangle(1705, 150 + (i*180), 1815, 260 + (i*180),al_map_rgb(200, 200, 0) , 7);
     }
     //restart rectangle
-    //al_draw_rectangle(860, 970, 990, 1020, al_map_rgb(200, 200, 0), 7);
     al_flip_display();
-
 }
 
 //calculates the score that is achieved by the player
 int addScore(int &total, int shirt, int pants, int shoes, int acc){
     total = shirt + pants + shoes + acc;
-
     return total;
 }
 
@@ -269,7 +219,6 @@ void reprintScreen(int &A, int &P, int &Shi, int &Sho, bool &shirtC, bool &pants
         P = 0;
         Shi = 0;
         Sho = 0;
-
         printf ("yay");
         loadGraphics();
         printHighScore(returnHighScore(highScore, totalScore));
@@ -281,5 +230,4 @@ void reprintScreen(int &A, int &P, int &Shi, int &Sho, bool &shirtC, bool &pants
         printScore(addScore(totalScore, Shi, P, Sho, A));
         displayRectangles();
         loadText ();
-
 }
